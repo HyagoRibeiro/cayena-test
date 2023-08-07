a96b3ac620f31fab7dfa3522155a50a9d6b34d20
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2023-08-07 12:57:34 -0300

    handle unexpect id into supplier detail

--- src/components/SupplierDetailPage/index.tsx
+++ src/components/SupplierDetailPage/index.tsx
@@ -9,7 +9,8 @@ import suppliersService from "../../services/suppliersService";
 function SupplierDetailPage() {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [supplierData, setSupplierData] = useState("");
+  const [supplierId, setSupplierId] = useState("");
+  const [error, setError] = useState(false);
 
   const [formData, setFormData] = useState({
     name: "",
@@ -28,23 +29,27 @@ function SupplierDetailPage() {
   });
 
   const getSuppliers = async (supplierId: string) => {
-    const { data } = await suppliersService.getById(supplierId);
-    setSupplierData(data.publicId);
-    setFormData({
-      name: data.name || "",
-      cnpj: data.cnpj || "",
-      phoneNumber: data.phoneNumber || "",
-      ownerName: data.ownerName || "",
-      ownerEmail: data.ownerEmail || "",
-      ownerPhoneNumber: data.ownerPhoneNumber || "",
-      address: data.address || "",
-      number: data.number || "",
-      complement: data.complement || "",
-      neighborhood: data.neighborhood || "",
-      city: data.city || "",
-      state: data.state || "",
-      zipCode: data.zipCode || "",
-    });
+    try {
+      const { data } = await suppliersService.getById(supplierId);
+      setSupplierId(data.publicId);
+      setFormData({
+        name: data.name || "",
+        cnpj: data.cnpj || "",
+        phoneNumber: data.phoneNumber || "",
+        ownerName: data.ownerName || "",
+        ownerEmail: data.ownerEmail || "",
+        ownerPhoneNumber: data.ownerPhoneNumber || "",
+        address: data.address || "",
+        number: data.number || "",
+        complement: data.complement || "",
+        neighborhood: data.neighborhood || "",
+        city: data.city || "",
+        state: data.state || "",
+        zipCode: data.zipCode || "",
+      });
+    } catch (error) {
+      setError(true);
+    }
   };
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -60,7 +65,7 @@ function SupplierDetailPage() {
 
     try {
       await api.put("https://test-case.cayena.io/suppliers", {
-        publicId: supplierData || "",
+        publicId: supplierId || "",
         ...formData,
       });
     } catch (error) {
@@ -79,169 +84,174 @@ function SupplierDetailPage() {
   return (
     <>
       <Menu />
-      <div className={`supplier-detail`}>
+      <div className={"supplier-detail"}>
         <button onClick={backToList()} className="btn-back">
-          {"< Voltar"}
+          &lt; Voltar
         </button>
-        <form className={"form"} onSubmit={handleSubmit}>
-          <div className="supplier-detail__header">
-            <h1>Supplier Details</h1>
-            <Button type="submit">Submit</Button>
-          </div>
-          <div className="form__block">
-            <div className="form__field">
-              <GenericInput
-                id="name"
-                type="text"
-                value={formData.name}
-                onChange={handleInputChange}
-                placeholder="name"
-                label="Name"
-                name="name"
-              />
+        {error && <div className="supplier-detail__error">
+          Um erro ocorreu. Tente novamente mais tarde.
+        </div>}
+        {!error && (
+          <form className={"form"} onSubmit={handleSubmit}>
+            <div className="supplier-detail__header">
+              <h1>Supplier Details</h1>
+              <Button type="submit">Submit</Button>
             </div>
-            <div className="form__field">
-              <GenericInput
-                id="cnpj"
-                name="cnpj"
-                type="text"
-                value={formData.cnpj}
-                onChange={handleInputChange}
-                placeholder="CNPJ"
-                label="CNPJ"
-              />
+            <div className="form__block">
+              <div className="form__field">
+                <GenericInput
+                  id="name"
+                  type="text"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  placeholder="name"
+                  label="Name"
+                  name="name"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="cnpj"
+                  name="cnpj"
+                  type="text"
+                  value={formData.cnpj}
+                  onChange={handleInputChange}
+                  placeholder="CNPJ"
+                  label="CNPJ"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="phoneNumber"
+                  name="phoneNumber"
+                  type="text"
+                  value={formData.phoneNumber}
+                  onChange={handleInputChange}
+                  placeholder="Phone Number"
+                  label="Phone Number"
+                />
+              </div>
             </div>
-            <div className="form__field">
-              <GenericInput
-                id="phoneNumber"
-                name="phoneNumber"
-                type="text"
-                value={formData.phoneNumber}
-                onChange={handleInputChange}
-                placeholder="Phone Number"
-                label="Phone Number"
-              />
-            </div>
-          </div>
 
-          <div className="form__block">
-            <h2>Owner</h2>
-            <div className="form__field">
-              <GenericInput
-                id="ownerName"
-                name="ownerName"
-                type="text"
-                value={formData.ownerName}
-                onChange={handleInputChange}
-                placeholder="Name"
-                label="Name"
-              />
+            <div className="form__block">
+              <h2>Owner</h2>
+              <div className="form__field">
+                <GenericInput
+                  id="ownerName"
+                  name="ownerName"
+                  type="text"
+                  value={formData.ownerName}
+                  onChange={handleInputChange}
+                  placeholder="Name"
+                  label="Name"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="ownerEmail"
+                  name="ownerEmail"
+                  type="text"
+                  value={formData.ownerEmail}
+                  onChange={handleInputChange}
+                  placeholder="Email"
+                  label="Email"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="ownerPhoneNumber"
+                  name="ownerPhoneNumber"
+                  type="text"
+                  value={formData.ownerPhoneNumber}
+                  onChange={handleInputChange}
+                  placeholder="Phone Number"
+                  label="Phone Number"
+                />
+              </div>
             </div>
-            <div className="form__field">
-              <GenericInput
-                id="ownerEmail"
-                name="ownerEmail"
-                type="text"
-                value={formData.ownerEmail}
-                onChange={handleInputChange}
-                placeholder="Email"
-                label="Email"
-              />
-            </div>
-            <div className="form__field">
-              <GenericInput
-                id="ownerPhoneNumber"
-                name="ownerPhoneNumber"
-                type="text"
-                value={formData.ownerPhoneNumber}
-                onChange={handleInputChange}
-                placeholder="Phone Number"
-                label="Phone Number"
-              />
-            </div>
-          </div>
 
-          <div className="form__block form__block--address">
-            <h2>Address</h2>
-            <div className="form__field form-block__address">
-              <GenericInput
-                id="address"
-                name="address"
-                type="text"
-                value={formData.address}
-                onChange={handleInputChange}
-                placeholder="Address"
-                label="Address"
-              />
+            <div className="form__block form__block--address">
+              <h2>Address</h2>
+              <div className="form__field form-block__address">
+                <GenericInput
+                  id="address"
+                  name="address"
+                  type="text"
+                  value={formData.address}
+                  onChange={handleInputChange}
+                  placeholder="Address"
+                  label="Address"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="number"
+                  name="number"
+                  type="text"
+                  value={formData.number}
+                  onChange={handleInputChange}
+                  placeholder="Number"
+                  label="Number"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="complement"
+                  name="complement"
+                  type="text"
+                  value={formData.complement}
+                  onChange={handleInputChange}
+                  placeholder="Complement"
+                  label="Complement"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="neighborhood"
+                  name="neighborhood"
+                  type="text"
+                  value={formData.neighborhood}
+                  onChange={handleInputChange}
+                  placeholder="Neighborhood"
+                  label="Neighborhood"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="city"
+                  name="city"
+                  type="text"
+                  value={formData.city}
+                  onChange={handleInputChange}
+                  placeholder="City"
+                  label="City"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="state"
+                  name="state"
+                  type="text"
+                  value={formData.state}
+                  onChange={handleInputChange}
+                  placeholder="State"
+                  label="State"
+                />
+              </div>
+              <div className="form__field">
+                <GenericInput
+                  id="zipCode"
+                  name="zipCode"
+                  type="text"
+                  value={formData.zipCode}
+                  onChange={handleInputChange}
+                  placeholder="Zip Code"
+                  label="Zip Code"
+                />
+              </div>
             </div>
-            <div className="form__field">
-              <GenericInput
-                id="number"
-                name="number"
-                type="text"
-                value={formData.number}
-                onChange={handleInputChange}
-                placeholder="Number"
-                label="Number"
-              />
-            </div>
-            <div className="form__field">
-              <GenericInput
-                id="complement"
-                name="complement"
-                type="text"
-                value={formData.complement}
-                onChange={handleInputChange}
-                placeholder="Complement"
-                label="Complement"
-              />
-            </div>
-            <div className="form__field">
-              <GenericInput
-                id="neighborhood"
-                name="neighborhood"
-                type="text"
-                value={formData.neighborhood}
-                onChange={handleInputChange}
-                placeholder="Neighborhood"
-                label="Neighborhood"
-              />
-            </div>
-            <div className="form__field">
-              <GenericInput
-                id="city"
-                name="city"
-                type="text"
-                value={formData.city}
-                onChange={handleInputChange}
-                placeholder="City"
-                label="City"
-              />
-            </div>
-            <div className="form__field">
-              <GenericInput
-                id="state"
-                name="state"
-                type="text"
-                value={formData.state}
-                onChange={handleInputChange}
-                placeholder="State"
-                label="State"
-              />
-            </div>
-            <div className="form__field">
-              <GenericInput
-                id="zipCode"
-                name="zipCode"
-                type="text"
-                value={formData.zipCode}
-                onChange={handleInputChange}
-                placeholder="Zip Code"
-                label="Zip Code"
-              />
-            </div>
-          </div>
-        </form>
+          </form>
+        )}
       </div>
     </>
   );
